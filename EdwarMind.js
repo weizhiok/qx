10aa179a28860f041cf53fb1a4606bcef69f50ed
weizhiok <165847692+weizhiok@users.noreply.github.com>
2024-04-03 05:48:20 +0800

    EdwarMind.js

---
+++ EdwarMind.js
@@ -0,0 +1,18 @@
+/******************************
+⚠️如果放远程，请把minded.js替换成运程链接⚠️
+*******************************
+%¥
+[rewrite_local]
+^https?:\/\/*\.edrawsoft\.com\/api\/user\/*.*? url script-response-body minded.js
+
+
+[mitm]
+hostname = *.edrawsoft.com
+%¥
+*******************************/
+var body=$response.body;
+body = body.replace(/1970-01-01/g,'2099-01-01');
+body = body.replace(/"fuser\":false/g,'"fuser":true');
+body = body.replace(/"license\":false/g,'"license":true');
+body = body.replace(/1900-01-01/g,'2099-01-01');
+$done(body);
