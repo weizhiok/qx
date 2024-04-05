7bb00d7ff9b1ebd1c88966eedbd5db94f14847bc
weizhiok <165847692+weizhiok@users.noreply.github.com>
2024-04-06 01:32:39 +0800

    EdwarMind.js

--- EdwarMind.js
+++ EdwarMind.js
@@ -1,19 +1,18 @@
 /******************************
-⚠️如果放远程，请把minded.js替换成运程链接⚠️
-
+⚠️如果放远程，请把EdwarMind.js替换成运程链接⚠️
 *******************************
 %¥
 [rewrite_local]
-^https?:\/\/.*\.edrawsoft\.com\/api\/user\/*.*? url script-response-body https://raw.githubusercontent.com/weizhiok/qx/main/EdwarMind.js
+^https?:\/\/userapi\.edrawsoft\.com\/api\/user\/*.*? url script-response-body https://raw.githubusercontent.com/weizhiok/qx/main/EdwarMind.js
 
 
 [mitm]
-hostname = *.edrawsoft.com
+hostname = userapi.edrawsoft.com
 %¥
 *******************************/
 var body=$response.body;
 body = body.replace(/1970-01-01/g,'2099-01-01');
-body = body.replace(/"fuser\":false/g,'"fuser":true');
-body = body.replace(/"license\":false/g,'"license":true');
+body = body.replace(/"fuser":false/g,'"fuser":true');
+body = body.replace(/"license":false/g,'"license":true');
 body = body.replace(/1900-01-01/g,'2099-01-01');
 $done(body);
