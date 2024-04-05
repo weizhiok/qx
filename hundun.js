4fa0d6539f5cc0bf32b0000ac6ed478a05114c5f
weizhiok <165847692+weizhiok@users.noreply.github.com>
2024-04-06 03:41:54 +0800

    hundun.js

--- hundun.js
+++ hundun.js
@@ -13,27 +13,5 @@ hostname = user.hundun.cn,course.hundun.cn
 %¥
 *******************************/
 var body=$response.body;
-body = body.replace(/\"play_tip_btn_msg\":\"\w+\"/g,'"play_tip_btn_msg":""');
-body = body.replace(/\"expire_text\":\"\w+\"/g,'"expire_text":""');
-body = body.replace(/\"play_tip_msg\":\"\w+\"/g,'"play_tip_msg":""');
-body = body.replace(/\"share_title\":\"\w+\"/g,'"share_title":""');
-body = body.replace(/\"playback_button\":\{.+\}\"is_praise"/g,'"is_praise"');
-body = body.replace(/\"price\":\"\d\"/g,'"price":"0"');
-body = body.replace(/\"can_view\":\d/g,'"can_view":1,');
-body = body.replace(/\"can_learn\":\d/g,'"can_learn":1,');
-body = body.replace(/\"is_unlock\":\d/g,'"is_unlock":1,');
-body = body.replace(/\"unlocked\":false/g,'"unlocked":true');
-body = body.replace(/\"allow_play\":\d/g,'"allow_play":1');
-body = body.replace(/\"is_visible\".+\"will_expire_value\"/g,'"is_visible":0,"will_expire_value");
-body = body.replace(/\"free_trial\":\d/g,'"free_trial":0');
-body = body.replace(/\allow_play_title\":\"\w+\"\,/g,'allow_play_title":"",');
-body = body.replace(/\"can_join_send_activity\":\d/g,'"can_join_send_activity":1,');
-body = body.replace(/\"is_free\":\d/g,'"is_free":1,');
-body = body.replace(/\"can_write_video_answer\":\d/g,'"can_write_video_answer":1,');
-body = body.replace(/\"is_buy\":\d/g,'"is_buy":1');
-body = body.replace(/\"is_free_4_user\":false/g,'"is_free_4_user":true');
-body = body.replace(/\"expire_time\":\"\d\d\d\d-\d\d-\d\d"/g,'"expire_time":"2099-01-01"');
-body = body.replace(/\"free_trial_info\".+\"i18n_directory"/g,'"i18n_directory"');
-body = body.replace(/\"playback_button\".+\"relate_object"/g,'"relate_object"');
 body = body.replace(/\{.+\"update_app\".+\}/g,'{}');
 $done(body);
