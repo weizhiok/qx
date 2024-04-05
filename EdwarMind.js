b50a1457bcea790097deae6da5d2219eae2a5d6b
weizhiok <165847692+weizhiok@users.noreply.github.com>
2024-04-06 03:03:53 +0800

    EdwarMind.js

--- EdwarMind.js
+++ EdwarMind.js
@@ -14,8 +14,7 @@ hostname = userapi.edrawsoft.com
 %¥
 *******************************/
 var body=$response.body;
-body = body.replace(/\d\d\d\d-\d\d-\d\d/g,'2099-01-01');
+body = body.replace(/19\d\d-\d\d-\d\d/g,'2099-01-01');
 body = body.replace(/"fuser\":\w\w\w\w\w/g,'"fuser":true');
 body = body.replace(/"license\":\w\w\w\w\w/g,'"license":true');
-body = body.replace(/\d\d\d\d-\d\d-\d\d/g,'2099-01-01');
 $done(body);
