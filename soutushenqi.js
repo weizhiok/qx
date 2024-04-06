16334be43e48ae7d80e2bc93f083160eee6cbd40
weizhiok <165847692+weizhiok@users.noreply.github.com>
2024-04-07 03:47:45 +0800

    Update soutushenqi.js

--- soutushenqi.js
+++ soutushenqi.js
@@ -1,1 +1,14 @@
 
+/******************************
+%¥
+[rewrite_local]
+^http?:\/\/wallpaper\.soutushenqi\.com\/api\/v?\/account\/token.*? url script-response-body xxx.js
+
+
+[mitm]
+hostname = wallpaper.soutushenqi.com
+%¥
+*******************************/
+var body=$response.body;
+body = body.replace(/vipType\":0/g,'vipType":1024');
+$done(body);
