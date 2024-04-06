0528bafc27aa09c74582b0af70453f578d29c6fc
weizhiok <165847692+weizhiok@users.noreply.github.com>
2024-04-07 03:21:49 +0800

    Update jianxun.js

--- jianxun.js
+++ jianxun.js
@@ -5,7 +5,8 @@
 
 [mitm] 
 hostname=api.tipsoon.com
-
 ***********************************/
+
+
 re('is_vip":false@expire_time":"\\d{4}','is_vip":true@expire_time":"2029')
 function re(){var e=$response.body;if(arguments[0].includes("@")){var n=arguments[0].split("@"),r=arguments[1].split("@");for(i=0;i<n.length;i++)var l=new RegExp(n[i],"g"),e=e.replace(l,r[i])}else{l=new RegExp(arguments[0],"g");e=e.replace(l,arguments[1])}$done(e)}
