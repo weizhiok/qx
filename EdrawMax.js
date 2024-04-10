a707abd0b38749ecdb8d6bf3b4d7a9dc586ebc01
weizhiok <165847692+weizhiok@users.noreply.github.com>
2024-04-10 23:36:21 +0800

    Update EdrawMax.js

--- EdrawMax.js
+++ EdrawMax.js
@@ -1,7 +1,7 @@
 /******************************
 %¥
 [rewrite_local]
-^https?:\/\/userapi\.edrawsoft.*\/api\/user\/\d+\/subscription\/max\?.*?|^https?:\/\/userapi\.edrawsoft.*\/api\/member\/\d+\/sales\/plan\?.*? url script-response-body https://raw.githubusercontent.com/weizhiok/qx/main/EdrawMax.js
+^https?:\/\/userapi\.edrawsoft.*\/api\/user\/\d+\/subscription\/max\?.*? url script-response-body https://raw.githubusercontent.com/weizhiok/qx/main/EdrawMax.js
 
 
 [mitm]
@@ -10,5 +10,4 @@ hostname = userapi.edrawsoft.com,userapi.edrawsoft.cn
 *******************************/
 var body=$response.body;
 body = body.replace(/\{\"status\"\:\"success\"\,\"data\"\:(null|\{\").+\"msg\"\:(null|\d)\}/g,'{"status":"success","data":{"exp":4070883661000,"mobile_exp":4070883661000,"online_exp":4070883661000,"win_exp":4070883661000,"mac_exp":4070883661000,"linux_exp":4070883661000,"group":true,"is_sub_vip":true,"version":1,"fuser":true,"platform_fuser":true,"now": 1111111111000},"msg":2}');
-body = body.replace(/\"is_trial\"\:true/g,'"is_trial":false');
 $done(body);
