37751fc6d1be7cf30e52c871b506744a85605bf4
weizhiok <165847692+weizhiok@users.noreply.github.com>
2024-04-11 06:48:24 +0800

    Update EdrawMax.js

--- EdrawMax.js
+++ EdrawMax.js
@@ -10,5 +10,5 @@ hostname = userapi.edrawsoft.com,userapi.edrawsoft.cn
 %¥
 *******************************/
 var body=$response.body;
-body = body.replace(/\{\"status\"\:\"success\"\,\"data\"\:(null|\{\").+\"msg\"\:(null|\d)\}/g,'{"status":"success","data":{"exp":4070883661000,"mobile_exp":4070883661000,"online_exp":4070883661000,"win_exp":4070883661000,"mac_exp":4070883661000,"linux_exp":4070883661000,"group":true,"is_sub_vip":true,"version":1,"fuser":true,"platform_fuser":true,"now": 1111111111000},"msg":2}');
+body = body.replace(/\{\"status\"\:\"success\"\,\"data\"\:(null|\{\").+\"msg\"\:(null|\d)\}/g,'{"status":"success","data":{"exp":4070883661000,"mobile_exp":4070883661000,"online_exp":4070883661000,"win_exp":4070883661000,"mac_exp":4070883661000,"linux_exp":4070883661000,"group":true,"is_sub_vip":true,"version":1,"fuser":true,"platform_fuser":true,"now":1704042061000},"msg":2}');
 $done(body);
