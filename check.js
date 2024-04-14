6869f8e8c414a7ec435dcdc3340c6ff78dd864f3
weizhiok <weizhiok@gmail.com>
2024-04-15 01:27:18 +0800

    Create check.js

---
+++ check.js
@@ -0,0 +1,32 @@
+
+/**
+ * @fileoverview Template to compose HTTP reqeuest.
+ * 
+ */
+
+const url = `https://raw.githubusercontent.com/weizhiok/qx/main/checkok.txt`;
+const method = `GET`;
+const headers = {
+'User-Agent' : `Mozilla/5.0 (iPhone; CPU iPhone OS 13_5_1 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/13.1.1 Mobile/15E148 Safari/604.1`,
+'Host' : `raw.githubusercontent.com`,
+'Connection' : `keep-alive`,
+'Accept-Language' : `zh-cn`,
+'Accept-Encoding' : `gzip, deflate, br`,
+'Accept' : `text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8`
+};
+const body = ``;
+
+const myRequest = {
+    url: url,
+    method: method,
+    headers: headers,
+    body: body
+};
+
+$task.fetch(myRequest).then(response => {
+    console.log(response.statusCode + "\n\n" + response.body);
+    $done();
+}, reason => {
+    console.log(reason.error);
+    $done();
+});
